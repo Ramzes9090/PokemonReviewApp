f4f51d4ece7c783a12a909da41e989c7a3493f0c
Daniel Śliwka <danielsliwkado@gmail.com>
2023-11-30 12:06:36 +0100

    add more functionality to ownerController

--- PokemonReviewApp/PokemonReviewApp/Controllers/OwnerController.cs
+++ PokemonReviewApp/PokemonReviewApp/Controllers/OwnerController.cs
@@ -48,5 +48,23 @@ namespace PokemonReviewApp.Controllers
             }
             return Ok(owner);
         }
+        [HttpGet("{ownerId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+            {
+                return NotFound();
+            }
+            var pokemon = _mapper.Map<List<PokemonDto>>(_ownerRepository.GetPokemonByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemon);
+        }
+
     }
 }
